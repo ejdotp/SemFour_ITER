0115b0e946f8c073d8cd0c3687ec9593b1aedf97
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-02-16 04:31:19 +0000

    X

--- Algorithm Design 2/1_Graph Problems/BFS.java
+++ Algorithm Design 2/1_Graph Problems/BFS.java
@@ -1,1 +1,36 @@
-// [Temporary Placeholder] bfs, dfs, mst, shortest path
+class BFS{
+    static class Node
+    {
+        int value;
+        Node left;
+        Node  right;
+        Node(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+    public static Node root;
+        
+    private Node addrec(Node current, int value)
+    {
+        if(current = null)
+            return new Node(value);
+        if(value<current.value)
+            current.left=addrec(current.left,value);
+        else if(value > root.value)
+            current.right = addrec(current.right, value);
+        else
+            return current;
+    }
+
+    public static void add(int value)
+    {
+        root = addrec(root,value);
+    }
+
+    
+
+
+}
