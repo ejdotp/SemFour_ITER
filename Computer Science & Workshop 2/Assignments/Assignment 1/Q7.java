fd18ef1886c9d0c9399eb05be7aa2d14fcb9042a
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-03-05 20:46:44 +0500

    Update Q7.java

--- Computer Science & Workshop 2/Assignments/Assignment 1/Q7.java
+++ Computer Science & Workshop 2/Assignments/Assignment 1/Q7.java
@@ -1,4 +1,4 @@
-// Consider a scenario where you are tasked with developing a simple banking application using Java, employing the concept of polymorphism. Your application should consist of three classes: Account, SavingsAccount, and CurrentAccount. The Account class serves as the base class with private attributes for account number and balance, along with abstract methods for deposit and withdrawal. The SavingsAccount class, a subclass of Account, should include an additional attribute for interest rate and override the deposit method to calculate interest, as well as override the withdrawal method to ensure a sufficient balance. Similarly, the CurrentAccount class, also a subclass of Account, should incorporate an overdraft limit attribute and override the withdrawal method to check the overdraft limit. Implement the classes as described, ensuring proper encapsulation and abstraction. Finally, create a BankingApplication class (Main) to demonstrate the polymorphic behavior by creating instances of both SavingsAccount and CurrentAccount, testing deposit and withdrawal operations, and displaying account details ?
+// Consider a scenario where you are tasked with developing a simple banking application using Java, employing the concept of polymorphism. Your application should consist of three classes: Account, SavingsAccount, and CurrentAccount. The Account class serves as the base class with private attributes for account number and balance, along with abstract methods for deposit and withdrawal. The SavingsAccount class, a subclass of Account, should include an additional attribute for interest rate and override the deposit method to calculate interest, as well as override the withdrawal method to ensure a sufficient balance. Similarly, the CurrentAccount class, also a subclass of Account, should incorporate an overdraft limit attribute and override the withdrawal method to check the overdraft limit. Implement the classes as described, ensuring proper encapsulation and abstraction. Finally, create a BankingApplication class (Main) to demonstrate the polymorphic behavior by creating instances of both SavingsAccount and CurrentAccount, testing deposit and withdrawal operations, and displaying account details.
 
 abstract class Account
 {
@@ -84,7 +84,7 @@ class CurrentAccount extends Account
     }
 }
 
-class BankingBANKER
+class BankingApplication
 {
     public static void main(String[] args)
     {
