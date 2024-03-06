a864a1c9272303b007601b3a6e98c533871b0892
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-03-06 05:18:14 +0000

    X

--- Computer Science & Workshop 2/Class Lectures/13_Generics/3_HashStuff/MarriedSet.java
+++ Computer Science & Workshop 2/Class Lectures/13_Generics/3_HashStuff/MarriedSet.java
@@ -7,15 +7,42 @@ class MarriedSet {
         LS.add("Three");
         LS.add("Four");
         LS.add("Five");
+
         Iterator<String> i=LS.iterator();
         while(i.hasNext()){
             System.out.println(i.next());
         } System.out.println(LS);
+
+        //Removing Specific element from hashset:
+        LS.remove("Ravi");
+        System.out.println("After invoking remove(object) method: " + LS);
+
+        HashSet<String> set1 = new HashSet<String>();
+        set1.add("Ajay");
+        set1.add("Gaurav");
+        LS.addAll(set1);
+        System.out.println("Updated List: " + LS);
+        //Removing all the new elements from the HashSet
+        LS.removeAll(set1);
+        System.out.println("After invoking removeAll(Object) method: " + LS);
+        //removing elements on the basis of specific condition
+        LS.removeIf(str->str.contains("Vijay"));
+        System.out.println("After invoking removeIf() method: " + LS);
+        //removing all elements available:
+        LS.clear();
+        System.out.println("After invoking clear() method: " + LS);
     }
 }
-/*One
+/*
+One
 Two
 Three
 Four
 Five
-[One, Two, Three, Four, Five] */
+[One, Two, Three, Four, Five]
+After invoking remove(object) method: [One, Two, Three, Four, Five]
+Updated List: [One, Two, Three, Four, Five, Gaurav, Ajay]
+After invoking removeAll(Object) method: [One, Two, Three, Four, Five]
+After invoking removeIf() method: [One, Two, Three, Four, Five]
+After invoking clear() method: []
+*/
