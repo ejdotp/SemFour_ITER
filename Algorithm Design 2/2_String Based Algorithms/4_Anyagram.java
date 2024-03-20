0950a42479d48a0d7d54e79ceeda911798345d71
ejdotp <jagadeeswar15j11@gmail.com>
2024-03-20 14:45:04 +0530

    x

--- Algorithm Design 2/2_String Based Algorithms/4_Anyagram.java
+++ Algorithm Design 2/2_String Based Algorithms/4_Anyagram.java
@@ -2,20 +2,23 @@
 
 class Anyagram
 {
-    static boolean areAnagram(String str1, String str2) {
-        if (str1.length() != str2.length()) {
+    static boolean areAnagram(String str1, String str2)
+    {
+        if (str1.length() != str2.length())
             return false;
-        }
+        
         int count[] = new int[256]; //character range
-        for (int i = 0; i < str1.length(); i++) {
+
+        for (int i = 0; i < str1.length(); i++)
+        {
             count[str1.charAt(i)]++;
             count[str2.charAt(i)]--;
         }
-        for (int i = 0; i < 256; i++) {
-            if (count[i] != 0) {
+
+        for (int i = 0; i < 256; i++)
+            if (count[i] != 0) 
                 return false;
-            }
-        }
+        
         return true;
     }
 
