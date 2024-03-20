ce540151b2e34ab400bf9c54e455bf37d40836ce
ejdotp <jagadeeswar15j11@gmail.com>
2024-03-20 14:06:52 +0530

    x

--- Algorithm Design 2/2_String Based Algorithms/1_Bruteforce.java
+++ Algorithm Design 2/2_String Based Algorithms/1_Bruteforce.java
@@ -1,18 +1,20 @@
 //Brute force algorithm for string search & count the occurence
 
-class Bruteforce {
-	// called function
-	public int bruteforce(String drop, String river) {
+class Bruteforce
+{
+	public int bruteforce(String drop, String river)
+	{
 		int m = drop.length();
 		int n = river.length();
 		int count = 0;
 
 		for (int i = 0; i <n-m; i++) {
 			int j = 0;
-			while ((j < m) && (river.charAt(i + j) == drop.charAt(j))) {
+			while ((j < m) && (river.charAt(i + j) == drop.charAt(j)))
 				j++;
-			}
-			if (j == m) {
+			
+			if (j == m)
+			{
 				System.out.println("Found at index: " + (i+1) + " upto " + (i+m));
 				count++;
 			}
@@ -21,7 +23,8 @@ class Bruteforce {
 		return count;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Bruteforce obj = new Bruteforce();
 		// text
 		String river = "Bro really said oh ethire the start position and end position duita jaga mention heichi. Let's add a random the here which we are to find.";
