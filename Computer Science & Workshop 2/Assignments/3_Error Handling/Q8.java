2e338548805dd451b30462fc8fd8a87c084e593e
ejdotp <jagadeeswar15j11@gmail.com>
2024-04-02 20:39:48 +0530

    x

--- Computer Science & Workshop 2/Assignments/3_Error Handling/Q8.java
+++ Computer Science & Workshop 2/Assignments/3_Error Handling/Q8.java
@@ -1,1 +1,21 @@
-//Write a Java program to find the quare root of integer numbers. Demonstrate the use of try-catchblock to handle ArithmeticException.
+/* Create a method that takes a string input and converts it to an integer. Handle
+NumberFormatException using try-catch block and prompt the user to enter a valid
+number upon exception. */
+public class Q8 {
+    public static void main(String[] args) {
+        try {
+            String str = "abc";
+            int num = convertToInt(str);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException caught");
+        }
+    }
+
+    public static int convertToInt(String str) {
+        return Integer.parseInt(str);
+    }    
+}
+//By :- Subhajyoti Prusty
+//2241016491
+//CSE-44
