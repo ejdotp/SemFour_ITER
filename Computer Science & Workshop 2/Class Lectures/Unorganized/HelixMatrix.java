e5a9b2bdf79fa85babacfb08a2cdcc16679d4a1f
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-04-04 06:27:19 +0000

    X

---
+++ Computer Science & Workshop 2/Class Lectures/Unorganized/HelixMatrix.java
@@ -0,0 +1,33 @@
+public class HelixMatrix {
+    
+    public static void main(String[] args) {
+        int[][] a = { { 1, 2, 3, 4, }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int k=0;
+        int m[]=new int[16];
+        for (int i = 0; i < 4; i++) {
+            if(i%2==0)
+            {
+               for(int j=0;j<4;j++)
+               {
+                 m[k]=a[i][j];
+                 System.out.println(m[k]);
+                 k++;
+               }
+            }
+            else{
+                for(int l=3;l>=0;l--)
+                {
+                    m[k]=a[i][l];
+                    System.out.println(m[k]);
+                    k++;
+                }
+
+            } 
+        }
+        for(int i=0;i<16;i++)
+        {
+            System.out.print(m[i]);
+        }
+       
+    }
+}
---
+++ Computer Science & Workshop 2/Class Lectures/Unorganized/SpiralMatrix.java
@@ -0,0 +1,38 @@
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        int s[]=new int[25];
+        int m=0;
+        int j=0;
+        int l=0;
+        int p=0;
+        for(int i=0;i<5;i++){
+            //Forward
+            for(j=0;j<4;j++){
+                s[m]=arr[i][j];
+                m++;
+               
+            }
+            //Downward
+            for(int k=0;k<4;k++){
+                s[m]=arr[k][j];
+                m++;
+            }
+            //Backward
+            for(l=4;l>0;l--){
+                s[m]=arr[j][l];
+                m++;
+            }
+            //Upward
+            for(p=4;p>0;p--){
+                s[m]=arr[p][l];
+                m++;
+            }
+            for(int q=0;q<25;q++){
+                System.out.print(s[q]+" ");
+            }
+        }
+       
+       
+    }
+}
