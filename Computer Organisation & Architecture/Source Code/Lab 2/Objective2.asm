0460b14b045a20a2f6520b014c183220c8788407
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-04-05 17:29:19 +0500

    >_<

--- Computer Organisation & Architecture/Source Code/Lab 2/Objective2.asm
+++ Computer Organisation & Architecture/Source Code/Lab 2/Objective2.asm
@@ -1,16 +1,16 @@
 ; E. Jagadeeswar Patro
 ; 2241016309
 mov ax, 0000h 
-mov ds, ax; ds=0000h  
+mov ds, ax 
 
-mov SI, 2000h
-mov al, [SI]     
-mov cl, 08
-mov ch, 0000h
+mov al, [2000H]     
+mov cl, 08h
 
 loop1: shr al, 01
        jc loop2
        inc ch
 loop2: dec cl
        jnz loop1
-hlt                
+       
+mov [2001h], ch
+hlt         
