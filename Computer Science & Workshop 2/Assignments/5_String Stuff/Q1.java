54ec103a04335ab84cc4436a6d6c2f46270697e4
ejdotp <jagadeeswar15j11@gmail.com>
2024-04-19 17:10:00 +0530

    >_<

--- Computer Science & Workshop 2/Assignments/5_String Stuff/Q1.java
+++ Computer Science & Workshop 2/Assignments/5_String Stuff/Q1.java
@@ -6,6 +6,42 @@
 class TestString {
     public static void main(String[] args) {
         String str1 = "Hello";
+        String str2 = new String("Hello");
+
+        System.out.println("Memory Usage: ");
+        System.out.println("str1 uses literal \"" + str1 + "\", created at compile time.");
+        System.out.println("str2 uses object \"" + str2 + "\", created at runtime.");
+
+        System.out.println("\nComparison: ");
+        if (str1 == str2) {
+            System.out.println("str1 and str2 are reference to same object.");
+        } else {
+            System.out.println("str1 and str2 are not reference to same object.");
+        }
+
+        if (str1.equals(str2)) {
+            System.out.println("The content of str1 and str2 are equal.");
+        } else {
+            System.out.println("The content of str1 and str2 are not equal.");
+        }
+    }
+}
+
+/*
+ * Output:
+ * Memory Usage:
+ * str1 uses literal "Hello", created at compile time.
+ * str2 uses object "Hello", created at runtime.
+ * 
+ * Comparison:
+ * str1 and str2 are not reference to same object.
+ * The content of str1 and str2 are not equal.
+ * 
+ * Also:
+ * 
+class TestString {
+    public static void main(String[] args) {
+        String str1 = "Hello";
         String str2 = new String(str1);
 
         System.out.println("Memory Usage: ");
@@ -26,7 +62,7 @@ class TestString {
         }
     }
 }
-/*
+
  * Output:
  * Memory Usage:
  * str1 uses literal "Hello", created at compile time.
@@ -34,5 +70,7 @@ class TestString {
  * 
  * Comparison:
  * str1 and str2 are not reference to same object.
- * The content of str1 and str2 are not equal.
+ * The content of str1 and str2 are equal.
+
  */
+
---
+++ Computer Science & Workshop 2/Assignments/5_String Stuff/Q3.java
@@ -0,0 +1,10 @@
+// 3.Write a Java program that uses StringBuffer to construct a simple text editor which can perform the following operations:
+//a.Append a given string to the existing text.
+//b.Insert a given string at a specified index within the existing text.
+//c.Delete a portion of text between two specified indices.
+//d.Reverse the entire text.
+//e.Replace a portion of the text between two specified indices with a given string.
+//Your program should display a menu with options to perform each of the above operations. 
+//After each operation, print the current state of the text. Also, display the current 
+//capacity and length of the StringBuffer after each operation to showcase its dynamic nature
+
