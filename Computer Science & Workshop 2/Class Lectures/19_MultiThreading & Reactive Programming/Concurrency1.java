06dd67a7ffbd97f52444c39286b3a57e65f817f1
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-05-07 07:47:39 +0000

    >_<

---
+++ Computer Science & Workshop 2/Class Lectures/19_MultiThreading & Reactive Programming/Concurrency1.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Concurrency1
+{
+    public static void main(String[] args)
+    {
+        ExecutorService excalliber = Executors.newFixedThreadPool(5); // executor named excalliber
+                                                                               // Creates a thread pool with 5 Threads
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread(" " + i);
+            excalliber.execute(worker); // Execute the worker
+        }
+        excalliber.shutdown(); // shutdown the executor once tasks are completed
+
+        while (!excalliber.isTerminated()) {
+
+        }
+        System.out.println("All tasks completed");
+    }
+}
+
+class WorkerThread implements Runnable
+{
+    private String message;
+
+    public WorkerThread(String s)
+    {
+        this.message = s;
+    }
+
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName() + " (Start) message = " + message);
+        processMessage(); // Simulating some work
+
+        System.out.println(Thread.currentThread().getName() + " (End)");
+    }
+
+    private void processMessage()
+    {
+        try {
+            Thread.sleep(2800); // Sleep for 2.8 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
