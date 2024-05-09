5b636dff4f5248cd3dc16ca387ccffc192afb96f
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-05-09 07:14:16 +0000

    >_<

--- Computer Science & Workshop 2/Class Lectures/Unorganized/MatrixMul.java
+++ Computer Science & Workshop 2/Class Lectures/Unorganized/MatrixMul.java
@@ -4,9 +4,9 @@ import java.util.Random;
 public class MatrixMul
 {   public static void main(String[] args)
     {
-        int[][] m1 = generateMatrix(3, 3);
+        int[][] m1 = generateMatrix(3, 2);
 		System.out.println(Arrays.deepToString(m1));
-        int[][] m2 = generateMatrix(3, 3);
+        int[][] m2 = generateMatrix(2, 4);
 		System.out.println(Arrays.deepToString(m2));
 		int[][] m3 = multiply(m1, m2);
         System.out.println(Arrays.deepToString(m3));
@@ -32,13 +32,15 @@ public class MatrixMul
         int c2 = B[0].length;
         int[][] C = new int[r1][c2];
         for  (int i = 0; i < r1; i++)
-            for(int j = 0; j < c2; j++)
+            for(int j = 0; j < c2; j++){
+                C[i][j] = 0;
                 for(int k = 0; k < c1; k++)
                     C[i][j] += A[i][k]*B[k][j];
+            }
         return C;
 	}
 }
 
-/*[[1832729, -1809404, 923482], [1425858, 989987, -1321211], [1076079, 266859, -656609]]
-[[567099, 1274952, 1346256], [368426, -697557, -399403], [397955, -235854, -2133496]]
-[[1477209465, 857744456, -12066700], [-982988797, 127002091, 752557287], [583685376, 633772479, -1517902033]]*/
+/*[[1534084, 1093954], [-1612899, 1271157], [-48039, 394886]]
+[[-23181, 765122, 1277753, -656997], [947750, 856612, 169854, -1408750]]
+[[505922328, 2026599760, -1490752032, -2080676720], [882109925, 852719470, 1860502411, -928179127], [1704643807, 860679754, 1396022981, -748163505]]*/
