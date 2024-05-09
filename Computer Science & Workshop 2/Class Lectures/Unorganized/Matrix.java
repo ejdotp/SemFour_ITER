277bad88bf06a15a883a39f1314cb0be564409d9
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-05-09 06:40:53 +0000

    >_<

--- Computer Science & Workshop 2/Class Lectures/Unorganized/Matrix.java
+++ Computer Science & Workshop 2/Class Lectures/Unorganized/Matrix.java
@@ -4,7 +4,8 @@ import java.util.Random;
 public class Matrix
 {   public static void main(String[] args)
     {
-        System.out.println(Arrays.deepToString(generateMatrix(3, 3)));
+        //System.out.println(Arrays.deepToString(generateMatrix(3, 3))); //driectly print but it will print in one line
+        printArray(generateMatrix(3, 3)); 
     }
 
     public static int[][] generateMatrix(int rows, int columns)
@@ -18,4 +19,23 @@ public class Matrix
 
         return matrix;
     }
+
+    public static void printArray(int[][] arr)    //not necessary if printing directly
+    {
+        if (arr == null) {
+            System.out.print("null");
+            return;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (int i = 0 ; i < arr.length ; i++ ) {
+            sb.append('\n');
+            sb.append(Arrays.toString(arr[i]));
+        }
+        sb.append("\n]");
+        System.out.println(sb.toString());
+    }
 }
+
+/*[[-242341, -1785124, 728511], [414515, -1886714, -1863612], [1612101, 735169, -1749393]] */
