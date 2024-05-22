55cfc421935c5c43e012cfd06d29735a6373470a
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-05-22 03:23:06 +0000

    >_<

---
+++ Computer Science & Workshop 2/Assignments/Project/ParallelVSSequential.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ParallelVSSequential
+{
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        
+    }
+}
