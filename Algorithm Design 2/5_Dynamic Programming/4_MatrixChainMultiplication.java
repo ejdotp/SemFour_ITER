ee1fb682bea90fa32859f1325e5e8acbb9bbf1a2
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-05-24 03:44:44 +0000

    >_<

--- Algorithm Design 2/5_Dynamic Programming/4_MatrixChainMultiplication.java
+++ Algorithm Design 2/5_Dynamic Programming/4_MatrixChainMultiplication.java
@@ -28,7 +28,7 @@ class Question4
 
     public static void main(String[] args)
     {
-        int[] arr = { 3, 2, 4, 2, 5 }; //A3x2 x B2x4 x C4x2 x D2x5
+        int[] arr = { 2, 3, 4, 1, 5 }; //A2x3 x B3x4 x C4x1 x D1x5
         int n = arr.length;
 
         for (int[] row : m)
@@ -38,4 +38,4 @@ class Question4
     }
 }
 /*Output:
-Minimum number of multiplications is 58*/
+Minimum number of multiplications is 28*/
