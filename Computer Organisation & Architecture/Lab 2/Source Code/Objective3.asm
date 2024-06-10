19f29e111bef7e7842286501ae3a5cee9340d741
ejdotp <jagadeeswar15j11@gmail.com>
2024-06-10 17:45:55 +0530

    >_<

--- Computer Organisation & Architecture/Lab 2/Source Code/Objective3.asm
+++ Computer Organisation & Architecture/Lab 2/Source Code/Objective3.asm
@@ -1,10 +1,13 @@
 ;E. Jagadeeswar Patro
 ;2241016309 
-
+         
+;MOVE BLOCK OF DATA FROM 1 LOCATION TO ANOTHER         
+         
 mov ax,0000h
-mov ds,ax
-mov SI,2000h
-mov DI,2010h
+mov ds,ax   
+
+mov SI,2000h ; SI = 2000H
+mov DI,2010h ; DI
 mov cl,05
 
 loop: mov bx,[SI]
