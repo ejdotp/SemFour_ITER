4d1ba0d571796a094b5dd25329e01b04d57586a7
ejdotp <jagadeeswar15j11@gmail.com>
2024-06-11 12:15:41 +0530

    >_<

--- Computer Organisation & Architecture/Lab 2/Source Code/Objective1.asm
+++ Computer Organisation & Architecture/Lab 2/Source Code/Objective1.asm
@@ -6,20 +6,27 @@ mov ds, ax
 
 mov SI, 2000h
 mov cl, [SI]   ;cl = count
+mov ch, 0000h  ;initialising for storing carry
 mov bx, cx
-mov ax, 0000h ;not necessary 
+mov ax, 0000h  ;not necessary 
 
 loop1: inc SI
        inc SI
        add ax, [SI]
-       jnc loop2  ; jump if not carry  
+       jnc loop2      ; jump if not carry  
+       inc ch
         
 loop2: dec cl
        jnz loop1
 
        inc SI
        inc SI
-       mov [SI], ax  ; sum stored   
+       mov [SI], ax   ; sum stored   
+       
+       inc SI
+       inc SI
+       mov [SI], ch   ;carry stored
+       mov dl, ch
        
        div bx
        inc SI
