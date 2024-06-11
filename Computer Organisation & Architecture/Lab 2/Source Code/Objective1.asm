0a175e56d57a6456d2857c1feb1b110e359df9f5
ejdotp <jagadeeswar15j11@gmail.com>
2024-06-11 12:07:53 +0530

    >_<

--- Computer Organisation & Architecture/Lab 2/Source Code/Objective1.asm
+++ Computer Organisation & Architecture/Lab 2/Source Code/Objective1.asm
@@ -1,30 +1,28 @@
 ;E. Jagadeeswar Patro
 ;2241016309
 
-mov ax,0000h
-mov ds,ax
+mov ax, 0000h
+mov ds, ax
 
-mov SI,2000h
-mov cl,[SI]   ;cl = count
-mov ch,00h
-mov bx,cx
-mov ax,0000h ;not necessary 
+mov SI, 2000h
+mov cl, [SI]   ;cl = count
+mov bx, cx
+mov ax, 0000h ;not necessary 
 
 loop1: inc SI
        inc SI
-       add ax,[SI]
-       jnc loop2  ; jump if not carry
-       inc ch  
+       add ax, [SI]
+       jnc loop2  ; jump if not carry  
         
 loop2: dec cl
        jnz loop1
-       inc SI
-       inc SI
-       mov [SI],ax  ; sum stored
-  
-       div bx
 
        inc SI
        inc SI
-       mov [SI],dx
+       mov [SI], ax  ; sum stored   
+       
+       div bx
+       inc SI
+       inc SI
+       mov [si], ax  ; Quotient/Average stored
 hlt               
